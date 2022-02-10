6ce0c5f1aad83f3fdb0a0712fe60672d0da7e23c
Alaa Hijazi <alaa.higazy@hotmail.com>
2022-02-10 12:42:48 +0000

    add new variable and a new function to showslide in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -184,3 +184,31 @@ buildQuiz();
 
 submitButton.addEventListener('click', showResults);
 })();
+
+//spliting quiz into page for each question
+
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+      previousButton.style.display = 'none';
+    }
+    else{
+      previousButton.style.display = 'inline-block';
+    }
+    if(currentSlide === slides.length-1){
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    }
+    else{
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
